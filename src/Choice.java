c2bc706cd628edff8248c6696106084cde4047f0
sawamk <projectpredel@gmail.com>
2024-11-17 14:49:56 +0300

    1

---
+++ src/Choice.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Choice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(new File("src/dictionary.txt"));
+
+        sc.close();
+
+    }
+}
