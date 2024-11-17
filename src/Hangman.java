e985e712aad68fda595e6e2cf8f418ba0af14020
sawamk <projectpredel@gmail.com>
2024-11-17 18:44:56 +0300

    1

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -37,18 +38,85 @@ public class Hangman {
                 continue CHOOSINESS;
             else
                 System.out.println(check.length);
-                System.out.println(Arrays.toString(check));
-                scanner.close();
-                break;
+            System.out.println(Arrays.toString(check));
+            scanner.close();
+            break;
         }
 
+        GAME:
+        while (true) {
+
+            Scanner scannerOfChoice = new Scanner(System.in);
+
+            CHOICE:
+            while (true) {
+                System.out.println("    Хотите начать новую игру? \n (Д) - Новая игра   (Н) - Выход");
+                String decision = scannerOfChoice.nextLine().toUpperCase();
+                System.out.println(decision);
+
+                if (decision.equals("Д"))
+                    break CHOICE;
+                else if (decision.equals("Н"))
+                    break GAME;
+                else
+                    System.out.println("Уважаемый пользователь, пожалуйста, введите только Д либо Н");
+                continue GAME;
+            }
 
 
 
 
+//
+//            System.out.println("Выберите уровень сложности \n (1) - легкий   (2) - средний   (3) - тяжелый");
+//            Scanner scannerOfDifficulty = new Scanner(System.in);
+//            String difficulty = scannerOfDifficulty.nextLine().toUpperCase();
+//            System.out.println(difficulty);
+//            scannerOfDifficulty.close();
+//
+//            DIFFICULTY:
+//            while (true) {
+//                if (difficulty.equals("1"))
+//                    break CHOICE;
+//                else if (decision.equals("Н"))
+//                    break GAME;
+//                else
+//                    System.out.println("Уважаемый пользователь, пожалуйста, введите только Д либо Н");
+//                continue GAME;
+//            }
+            System.out.println(hiddenWord);
+            char[] wordAsChars = hiddenWord.toCharArray();
 
-        System.out.println(hiddenWord);
-        char[] wordAsChars = hiddenWord.toCharArray();
+
+
+            char letter = '*';
+
+
+
+            while (true) {
+
+                System.out.println("Введите букву русского алфавита");
+                String letterInput = scannerOfChoice.nextLine().toUpperCase();
+
+
+                if (!letterInput.isEmpty() )
+                    letter = letterInput.charAt(0);
+                else
+                    System.out.println("Вы ничего не ввели");
+
+                if (letter >= 'А' && letter <= 'Я' || letter == 'Ё')
+                break;
+                else
+                    System.out.println("Вы ввели не букву, попробуйте еще раз");
+                continue;
+            }
+
+
+            System.out.println(letter);
+
+
+            break GAME;
+        }
+
     }
 
 }
